e677d10e804b8d48da96ec31007894a2549b69ea
万松涛 <2294215581@qq.com>
2020-11-16 16:04:51 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+//导入组件库
+import Vant from 'vant'
+//引入vant组件的css文件
+import 'vant/lib/index.css'
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: function (h) { return h(App) }
-}).$mount('#app')
+}).$mount('#app');
+
+//注册vant ui组件库的标签
+Vue.use(Vant);
