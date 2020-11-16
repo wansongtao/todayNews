6abde35a10aa1068da1662a85bf83f9356f5a080
万松涛 <2294215581@qq.com>
2020-11-16 14:54:33 +0800

    新建一个输入框组件

---
+++ src/components/myinput.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" :value="value" @input="updateValue" :placeholder="placeholder">
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: [String, Number],
+            default: ''
+        },
+        placeholder: String
+    },
+    methods: {
+        updateValue(event) {
+            //将修改后的值发送给父组件
+            this.$emit('input', event.target.value);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
