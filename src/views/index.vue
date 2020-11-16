a72a21599b9f75550759678f389fcc6b02e17496
万松涛 <2294215581@qq.com>
2020-11-16 14:35:41 +0800

    vue.cli项目开始工作

---
+++ src/views/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1>我的第一个vue.cli项目</h1>
+      <p>不过是大梦一场空</p>
+      <p>不过是孤影照惊鸿</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+    div {
+        width: 300px;
+        height: 150px;
+        margin: 100px auto;
+        h1 {
+            font-family: 'kaiti', 'songti', sans-serif;
+            font-size: 24px;
+        }
+        p {
+            font-size: 16px;
+        }
+    }
+</style>
