e0117fda8aa8a90b9a9f476d30c468a7e50834d2
万松涛 <2294215581@qq.com>
2020-11-17 21:41:21 +0800

    导入Vant-ui的toast提示框

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import store from './store'
 import Vant from 'vant'
 //引入vant组件的css文件
 import 'vant/lib/index.css'
+import { Toast } from 'vant';
 
 //引入axios处理请求
 import axios from 'axios'
@@ -21,6 +22,7 @@ new Vue({
 
 //注册vant ui组件库的标签
 Vue.use(Vant);
+Vue.use(Toast);
 
 //挂到原型上
 Vue.prototype.$axios = axios;
