f5ad393af7c6985303e57159cc315c34816c462d
万松涛 <2294215581@qq.com>
2020-11-17 21:43:40 +0800

    添加toast的引入注释

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 //导入组件库
-import Vant from 'vant'
-//引入vant组件的css文件
-import 'vant/lib/index.css'
+import Vant from 'vant';
+//引入vant组件的css文件j
+import 'vant/lib/index.css';
+//导入toast轻提示框
 import { Toast } from 'vant';
 
 //引入axios处理请求
-import axios from 'axios'
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 
@@ -22,6 +23,7 @@ new Vue({
 
 //注册vant ui组件库的标签
 Vue.use(Vant);
+//注册toast轻提示框，注册后会自定挂载在vue的原型上，可以通过this.$toast调用
 Vue.use(Toast);
 
 //挂到原型上
