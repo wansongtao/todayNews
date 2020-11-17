b6acefb2f85e09420b6f5e8d11cf87f16c5727b4
万松涛 <2294215581@qq.com>
2020-11-17 20:20:44 +0800

    添加一个按钮组件

---
+++ src/components/AuthBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="btn">
+      {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: '登录'
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.btn {
+    height: 48 / 360 * 100vw;
+    background: #cc3300;
+    line-height: 48 / 360 * 100vw;
+    font-size: 18 / 360 * 100vw;
+    color: #fff;
+    border-radius: 24 / 360 * 100vw;
+}
+</style>
