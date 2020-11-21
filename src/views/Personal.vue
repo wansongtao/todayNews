24623455733b2bc5e3d6ee1a7bab571a0fa7364a
万松涛 <2294215581@qq.com>
2020-11-21 10:13:00 +0800

    引入修改后的组件

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -3,9 +3,13 @@
     <!-- 用户信息 -->
     <router-link to="/personal/useredit">
       <div class="userinfo">
-        <img :src="head_img" alt="头像" />
+        <img :src="head_img" alt="头像" v-if="head_img === 'string'" />
+        <img src="../assets/user.jpg" alt="头像" v-else />
         <div class="username">
-          <p><i :class="['iconfont', gender === 0 ? 'iconnan1' : 'iconnv']"></i>{{nickname}}</p>
+          <p>
+            <i :class="['iconfont', gender === 0 ? 'iconnan1' : 'iconnv']"></i
+            >{{ nickname }}
+          </p>
           <span class="date">2020-11-20</span>
         </div>
         <span class="iconfont iconright"></span>
@@ -14,29 +18,35 @@
 
     <!-- 工具列表 -->
     <div class="barlist">
-      <!-- <router-link to="/">
-        <div class="destext">我的关注</div>
-        <div class="detailinfo">
-          关注的用户<i class="iconfont iconright"></i>
-        </div>
-      </router-link> -->
-      <personal-list
-        url="/personal/attention"
-        listtext="我的关注"
-        detailtext="关注的用户"
-      ></personal-list>
-      <personal-list
-        url="/personal/commentlist"
-        listtext="我的跟帖"
-        detailtext="跟帖/回复"
-      ></personal-list>
-      <personal-list
-        url="/personal/collect"
-        listtext="我的收藏"
-        detailtext="文章/视频"
-      ></personal-list>
-      <personal-list url="/login" listtext="退出登录" @click.native="handlerClick"></personal-list>
-      <personal-list url="/" listtext="返回首页"></personal-list>
+      <router-link to="/personal/attention">
+        <personal-list
+          listtext="我的关注"
+          detailtext="关注的用户"
+        ></personal-list>
+      </router-link>
+
+      <router-link to="/personal/commentlist">
+        <personal-list
+          listtext="我的跟帖"
+          detailtext="跟帖/回复"
+        ></personal-list>
+      </router-link>
+      <router-link to="/personal/collect">
+        <personal-list
+          listtext="我的收藏"
+          detailtext="文章/视频"
+        ></personal-list>
+      </router-link>
+
+      <router-link to="/"
+        ><personal-list listtext="返回首页"></personal-list
+      ></router-link>
+      <router-link to="/login">
+        <personal-list
+          listtext="退出登录"
+          @click.native="handlerClick"
+        ></personal-list>
+      </router-link>
     </div>
   </div>
 </template>
@@ -45,38 +55,34 @@
 import personalList from "../components/personalList";
 export default {
   data() {
-      return {
-          gender: 1,
-          head_img: '../assets/user.jpg',
-          nickname: ''
-      }
+    return {
+      gender: 1,
+      head_img: "",
+      nickname: "",
+    };
   },
   components: {
     "personal-list": personalList,
   },
   created() {
-      this.$axios.get('/user/' + localStorage.userId, {
-        headers: {
-            Authorization: localStorage.token
-        }
-      }).then(res => {
-          const {gender, head_img, nickname, username} = res.data.data;
+    this.$axios.get("/user/" + localStorage.userId).then((res) => {
+      const { gender, head_img, nickname, username } = res.data.data;
 
-          this.gender = gender;
-          if(head_img) {
-              if(head_img.indexof('http') == -1) {
-                  this.head_img = sessionStorage.baseURL + head_img;
-              }
-          }
-          this.nickname = nickname || username;
-      });
+      this.gender = gender;
+      if (head_img) {
+        if (head_img.indexof("http") == -1) {
+          this.head_img = sessionStorage.baseURL + head_img;
+        }
+      }
+      this.nickname = nickname || username;
+    });
   },
   methods: {
-      handlerClick() {
-          delete localStorage.token;
-          delete localStorage.userId;
-      }
-  }
+    handlerClick() {
+      delete localStorage.token;
+      delete localStorage.userId;
+    },
+  },
 };
 </script>
 
