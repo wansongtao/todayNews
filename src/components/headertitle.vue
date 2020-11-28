8ac4c21612ae9349ff7fdbb5f59cdf12d987d171
万松涛 <2294215581@qq.com>
2020-11-28 12:14:29 +0800

    添加一个头部标题组件

---
+++ src/components/headertitle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h4><i class="iconfont icondayu" @click="$emit('back')"></i>{{title}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+h4 {
+  position: relative;
+  text-align: center;
+  font-size: 16 / 360 * 100vw;
+  line-height: 40 / 360 * 100vw;
+  color:#fff;
+  background: #d43d3d;
+
+  i {
+    position: absolute;
+    top: 0;
+    left: 15 / 360 * 100vw;
+    font-size: 16 / 360 * 100vw;
+    color: #fff;
+  }
+}
+</style>
